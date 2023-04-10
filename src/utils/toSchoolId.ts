a6d078676b983436253f34bd19aff39cab71a041
Lai chane an <m020203@gms.tku.edu.tw>
2023-04-10 18:20:13 -0500

    Fix

--- src/utils/toSchoolId.ts
+++ src/utils/toSchoolId.ts
@@ -2,5 +2,6 @@ import generateUUID from "@/utils/generateUUID"
 import config from "@/config"
 
 export default (v2SiteInfoHashedId: string): string => {
-  return config.schoolId ?? generateUUID(v2SiteInfoHashedId)
+  const schoolId = (config as any).schoolId
+  return schoolId ?? generateUUID(v2SiteInfoHashedId)
 }
