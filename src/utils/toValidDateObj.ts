37fbec46ba1ee3382eb8ec387321fd191397bf8c
Lai chane an <m020203@gms.tku.edu.tw>
2023-06-04 21:51:21 -0500

    fix

--- src/utils/toValidDateObj.ts
+++ src/utils/toValidDateObj.ts
@@ -1,6 +1,13 @@
+import moment from "moment";
+
 export default (date: Date | null): Date | null => {
   if (date?.toString() === "0000-00-00 00:00:00") {
     return new Date()
+  }
+  try {
+    date?.toISOString()
+  } catch (e) {
+    return moment(date).toDate();
   }
   return date ?? null
 };
