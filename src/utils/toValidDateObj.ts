34200ac1a78d34228eeb18c97ff0cb00aa69f54f
Lai chane an <m020203@gms.tku.edu.tw>
2023-06-04 22:43:05 -0500

    fix

--- src/utils/toValidDateObj.ts
+++ src/utils/toValidDateObj.ts
@@ -4,10 +4,11 @@ export default (date: Date | null): Date | null => {
   if (date?.toString() === "0000-00-00 00:00:00") {
     return new Date()
   }
-  try {
-    date?.toISOString()
-  } catch (e) {
+  if (!moment(date).isValid()) {
+    return null;
+  }
+  if (typeof date === "string") {
     return moment(date).toDate();
   }
-  return date ?? null
+  return date ?? null;
 };
