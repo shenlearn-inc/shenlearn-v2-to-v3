727fe680dfd62ccd2c8c1ee5d29a9dd1be7c7cd7
Lai chane an <m020203@gms.tku.edu.tw>
2023-08-07 14:11:20 -0500

    fix

--- src/config/index.ts
+++ src/config/index.ts
@@ -149,7 +149,7 @@ export default {
     planId: plans.pro,
     roles: roles.pro,
     isDeleteContactor: false,
-    isHandleDuplicateHashedId: true,
+    isHandleDuplicateHashedId: false,
   })) as Site[],
 
   // !必改
