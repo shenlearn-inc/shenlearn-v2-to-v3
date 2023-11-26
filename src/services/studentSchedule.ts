7657d8dcfc1677583993d24e38036a2785f5074c
Lai chane an <m020203@gms.tku.edu.tw>
2023-11-26 12:31:28 -0600

    feat: migrate goldmadelsh

--- src/services/studentSchedule.ts
+++ src/services/studentSchedule.ts
@@ -48,7 +48,7 @@ export default async (trxs: Trxs) => {
     const v2TeacherMap = _.keyBy(v2Teachers, 'id')
 
     await createStudentSchedules(
-      v2StudentSchedules.filter((s) => !!s.handleAt).map(s => {
+      v2StudentSchedules.filter((s) => !!s.handleAt && s.studentId in v2StudentMap && s.courseId in v2CourseMap).map(s => {
         const studentId  = toStudentId(v2StudentMap[s.studentId].hashedId)
         const clazzId = toClazzId(v2CourseMap[s.courseId].hashedId)
         const type = toStudentScheduleType(s.event)
